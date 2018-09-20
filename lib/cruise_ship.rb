278b14d4a6c2a1239e859251b65facc3ff61654f
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 15:31:57 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,8 +6,7 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
-  # add the code snippet here!
+
   def select_winner(passengers)
     winner = ""
   passengers.each do |suite, name|
