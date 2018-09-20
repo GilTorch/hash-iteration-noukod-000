0730c193d2bb159631e749b86f91f659de8b888c
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 15:31:36 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,13 +11,4 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def select_winner(passengers)
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
 
-winner
-end
