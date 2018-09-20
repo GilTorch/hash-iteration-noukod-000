fd86aea41a48a2e30f64930521cad118a4e0b553
Gilbert Torchon <gilbert@noukod.com>
2018-09-20 15:19:28 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,9 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each_with_index do |age,name|
+    puts "Happy Birthday #{name}! You are #{age} years old!"
+  end
 end
 
 
